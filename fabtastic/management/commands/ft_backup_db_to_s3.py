a1b21c0b96235e6f65b408c413add4a0812e393f
Greg Taylor <gtaylor@duointeractive.com>
2011-02-11 16:18:07 -0500

    S3 AWS credential should be pulled from settings.py, not fabfile env.

--- fabtastic/management/commands/ft_backup_db_to_s3.py
+++ fabtastic/management/commands/ft_backup_db_to_s3.py
@@ -1,8 +1,8 @@
 import os
 
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
-from fabric.api import *
 from fabtastic import db
 from fabtastic.util.aws import get_s3_connection
 
@@ -19,7 +19,7 @@ class Command(BaseCommand):
 
         print "Uploading to S3."
         conn = get_s3_connection()
-        bucket = conn.create_bucket(env.S3_DB_BACKUP['BUCKET'])
+        bucket = conn.create_bucket(settings.S3_DB_BACKUP['BUCKET'])
         key = bucket.new_key(dump_filename)
         key.set_contents_from_filename(dump_file_path)
         print "S3 DB backup complete."
