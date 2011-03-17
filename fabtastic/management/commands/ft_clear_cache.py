eac50b1603c4a8c289547c4fb3874b8e3e178ae1
Greg Taylor <gtaylor@duointeractive.com>
2011-03-17 14:34:10 -0400

    Use Django 1.2's new cache.clear() method instead of the hacky old way.

--- fabtastic/management/commands/ft_clear_cache.py
+++ fabtastic/management/commands/ft_clear_cache.py
@@ -1,8 +1,8 @@
 from django.core.management.base import BaseCommand
-from fabtastic.util.cache import clear_django_cache
+from django.core.cache import cache
 
 class Command(BaseCommand):
     help = "Flushes the Django cache."
 
     def handle(self, *args, **options):
-        clear_django_cache()            
+        cache.clear()
