3da286b934dcb9da5f06a8e19342f74079e432ef
Greg Taylor <gtaylor@duointeractive.com>
2011-08-15 15:49:13 -0400

    Make backing up to S3 use a year/month/date structure for backups, for S3 clients that 'fake' directory structures.

--- fabtastic/management/commands/ft_backup_db_to_s3.py
+++ fabtastic/management/commands/ft_backup_db_to_s3.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -21,8 +22,20 @@ class Command(BaseCommand):
         print "Uploading to S3."
         conn = get_s3_connection()
         bucket = conn.create_bucket(settings.S3_DB_BACKUP['BUCKET'])
-        key = bucket.new_key(dump_filename)
+        now = datetime.datetime.now()
+        s3_path = '%d/%d/%d/%s' % (
+            now.year,
+            now.month,
+            now.day,
+            dump_filename,
+        )
+        key = bucket.new_key(s3_path)
         key.set_contents_from_filename(dump_file_path)
+        bucket.copy_key(
+            'latest_db.sql.tar.bz2',
+            settings.S3_DB_BACKUP['BUCKET'],
+            s3_path,
+        )
         print "S3 DB backup complete."
 
         # Clean up the temporary download file.
