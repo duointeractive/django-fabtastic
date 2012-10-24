3b47e78cd72b329f468c44891967548e8b410f6d
Greg Taylor <gtaylor@l11solutions.com>
2012-10-24 12:10:36 -0400

    Adding --nopinput to collectstatic.

--- fabtastic/fabric/commands/c_compressor.py
+++ fabtastic/fabric/commands/c_compressor.py
@@ -8,5 +8,5 @@ def collectstatic(roles='webapp_servers'):
     if _current_host_has_role(roles) and not env.already_media_synced:
         print("=== SYNCING STATIC MEDIA WITH S3 ===")
         with cd(env.REMOTE_CODEBASE_PATH):
-            run("workon %s && ./manage.py collectstatic" % env.REMOTE_VIRTUALENV_NAME)
+            run("workon %s && ./manage.py collectstatic --noinput" % env.REMOTE_VIRTUALENV_NAME)
         env.already_media_synced = True
